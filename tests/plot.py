2e884ca6db3f00e2e57e9c6547a0b1516388a747
Peter Polidoro <peterpolidoro@gmail.com>
2015-07-14 16:05:01 -0400

    Initial data plot.

--- tests/plot.py
+++ tests/plot.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     ax1.set_xlabel('volume (ml)')
     ax1.set_ylabel('mean signal (ADC units)')
 
-    ax1.text(0.5,400,r'$s = c_0v^2 + c_1v + c_2$',fontsize=20)
+    ax1.text(0.5,410,r'$s = c_0v^2 + c_1v + c_2$',fontsize=20)
     ax1.grid(True)
 
     coefficients_A1_ADC = numpy.polyfit(volumes,A1_ADC,order)
@@ -36,14 +36,14 @@ if __name__ == '__main__':
     polynomial_A1_ADC = numpy.poly1d(coefficients_A1_ADC)
     ys_A1_ADC = polynomial_A1_ADC(volumes)
     ax1.plot(volumes,ys_A1_ADC,'r--')
-    ax1.text(0.5,360,str(coefficients_A1_ADC),fontsize=18,color='r')
+    ax1.text(0.5,390,str(coefficients_A1_ADC),fontsize=18,color='r')
 
     coefficients_A4_ADC = numpy.polyfit(volumes,A4_ADC,order)
     coefficients_A4_ADC = [round(i,round_digits) for i in coefficients_A4_ADC]
     polynomial_A4_ADC = numpy.poly1d(coefficients_A4_ADC)
     ys_A4_ADC = polynomial_A4_ADC(volumes)
     ax1.plot(volumes,ys_A4_ADC,'g--')
-    ax1.text(0.5,380,str(coefficients_A4_ADC),fontsize=18,color='g')
+    ax1.text(0.5,400,str(coefficients_A4_ADC),fontsize=18,color='g')
 
     # Axis 2
     order = 2
@@ -55,20 +55,29 @@ if __name__ == '__main__':
     ax2.set_ylabel('volume (ml)')
     ax2.grid(True)
 
-    ax2.text(270,6.0,r'$v = c_0s^2 + c_1s + c_2$',fontsize=20)
+    ax2.text(270,6.5,r'$v = c_0s^2 + c_1s + c_2$',fontsize=20)
 
     coefficients_A1_ADC = numpy.polyfit(A1_ADC,volumes,order)
     coefficients_A1_ADC = [round(i,round_digits) for i in coefficients_A1_ADC]
     polynomial_A1_ADC = numpy.poly1d(coefficients_A1_ADC)
     ys_A1_ADC = polynomial_A1_ADC(A1_ADC)
     ax2.plot(A1_ADC,ys_A1_ADC,'r--')
-    ax2.text(270,5.0,str(coefficients_A1_ADC),fontsize=18,color='r')
+    ax2.text(270,6.0,str(coefficients_A1_ADC),fontsize=18,color='r')
 
     coefficients_A4_ADC = numpy.polyfit(A4_ADC,volumes,order)
     coefficients_A4_ADC = [round(i,round_digits) for i in coefficients_A4_ADC]
     polynomial_A4_ADC = numpy.poly1d(coefficients_A4_ADC)
     ys_A4_ADC = polynomial_A4_ADC(A4_ADC)
     ax2.plot(A4_ADC,ys_A4_ADC,'g--')
-    ax2.text(270,4.0,str(coefficients_A4_ADC),fontsize=18,color='g')
+    ax2.text(270,5.5,str(coefficients_A4_ADC),fontsize=18,color='g')
+
+    # coefficients_array =  a = numpy.vstack((coefficients_A1_ADC,coefficients_A4_ADC))
+    # coefficients_average = numpy.average(coefficients_array,0)
+    # polynomial_average = numpy.poly1d(coefficients_average)
+    # ys_average_A1_ADC = polynomial_average(A1_ADC)
+    # ax2.plot(A1_ADC,ys_average_A1_ADC,'b')
+    # ys_average_A4_ADC = polynomial_average(A4_ADC)
+    # ax2.plot(A4_ADC,ys_average_A4_ADC,'b')
+    # ax2.text(270,5.0,str(coefficients_average),fontsize=18,color='b')
 
     plot.show()
